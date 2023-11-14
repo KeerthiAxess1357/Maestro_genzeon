18c882ee4dd5a54f6f2555d463aadbf8b596017a
Keerthi@1357 <keerthana.t@axesstechnology.in>
2023-11-14 12:51:31 +0530

    hello

---
+++ src/main/java/Dominos/pom/Cart.java
@@ -0,0 +1,11 @@
+package Dominos.pom;
+
+import org.openqa.selenium.By;
+
+public class Cart {
+
+    public By checkoutbtn = By.xpath("//button[@data-label=\"miniCartCheckout\"]");
+
+
+//hvshabjk
+}
